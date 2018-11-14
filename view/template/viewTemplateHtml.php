276fa7a6eb7a536036052628c018644e8b787826
manoxiah <votre@email.commaxime@LAPTOP-FQ7QO782 MINGW64 ~ (mast>
2018-11-15 00:36:45 +0100

    Mise en place de la couleur white au bouton de navigation

--- view/template/viewTemplateHtml.php
+++ view/template/viewTemplateHtml.php
@@ -18,12 +18,28 @@
             <div class="col-lg-3 navbar-header logo">
               <span>Jean FORTEROCHE</span>
             </div>
-            <ul class="col-lg-6 nav navbar-nav pull-right">
-              <li class="col-lg-2"><a href="./index.php?callPage=home&stateChapter=1"><span class="buttonNavColor">Accueil</span></a></li>
-              <li class="col-lg-3"> <a href="./index.php?callPage=chapterDisplayListChapter&stateChapter=1">Les chapitres</a></li>
-              <li class="col-lg-2"> <a href="./index.php?callPage=about">A propos</a></li>
-              <li class="col-lg-2"> <a href="./index.php?callPage=contact">Contact</a></li>
-            </ul>
+              <ul class="col-lg-6 nav navbar-nav pull-right">
+                  <li class="col-lg-2">
+                      <a href="./index.php?callPage=home&stateChapter=1&colorButtonNavHome=0">
+                          <span class="<?php if ((isset($_GET['colorButtonNavHome'])) and ($_GET['colorButtonNavHome'] == 0)) { echo 'colorButtonNav';} else { echo '';}?>">Accueil</span>
+                      </a>
+                  </li>
+                  <li class="col-lg-3">
+                      <a href="./index.php?callPage=chapterDisplayListChapter&stateChapter=1&colorButtonNavChapter=0">
+                          <span class="<?php if ((isset($_GET['colorButtonNavChapter'])) and ($_GET['colorButtonNavChapter'] == 0)) { echo 'colorButtonNav';} else { echo '';}?>">Les chapitres</span>
+                      </a>
+                  </li>
+                  <li class="col-lg-2">
+                      <a href="./index.php?callPage=about&colorButtonNavAbout=0">
+                          <span class="<?php if ((isset($_GET['colorButtonNavAbout'])) and ($_GET['colorButtonNavAbout'] == 0)) { echo 'colorButtonNav';} else { echo '';}?>">A propos</span>
+                      </a>
+                  </li>
+                  <li class="col-lg-2">
+                      <a href="./index.php?callPage=contact&colorButtonNavContact=0">
+                          <span class="<?php if ((isset($_GET['colorButtonNavContact'])) and ($_GET['colorButtonNavContact'] == 0)) { echo 'colorButtonNav';} else { echo '';}?>">Contact</span>
+                      </a>
+                  </li>
+              </ul>
           </nav>
       </header>
 
